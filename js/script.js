8cf5d88ec9985057a6fa26da19e76cd92815d8c5
sulianggang <sulianggang@askingdata.com>
2017-02-21 22:14:49 +0800

    结构性功能完成

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(function() {
     function courseHeight() {
         var height = $(window).height() - 180;
         $('.course-main').css('min-height',height + 'px');
+        $('.tab-content').css('min-height',height - 150 + 'px');
     }
     courseHeight()
     $(window).resize(function(){
